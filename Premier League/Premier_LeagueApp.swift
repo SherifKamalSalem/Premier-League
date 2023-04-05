f584d77ede43b86be98f3c19e69feb4e225df56f
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-05 02:18:15 +0200

    Initial Commit

---
+++ Premier League/Premier_LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Premier_LeagueApp.swift
+//  Premier League
+//
+//  Created by Sherif Kamal on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Premier_LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
