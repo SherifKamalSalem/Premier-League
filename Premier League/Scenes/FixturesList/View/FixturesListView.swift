8699b46d195c86735e9db5ce959979e1bddf5273
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-12 21:00:51 +0200

    Setup fixtures list and handling view model presentation status

--- Premier League/Scenes/FixturesList/View/FixturesListView.swift
+++ Premier League/Scenes/FixturesList/View/FixturesListView.swift
@@ -18,19 +18,64 @@ struct FixturesListView<ViewModel: FixturesListViewModelProtocol>: View {
         NavigationStack {
             content
                 .navigationBarTitle("Fixtures".localized)
+                .navigationBarItems(trailing: toggleFavoritesOnlyButton)
         }
     }
     
     private var content: some View {
         Group {
-            fixturesList
+            switch viewModel.state.value {
+            case .loading:
+                ProgressView()
+            case .empty:
+                emptyResultView
+            case .finished:
+                fixturesList
+            default:
+                emptyResultView
+            }
         }
     }
     
     private var fixturesList: some View {
         List {
-            
+            let fixtures = viewModel.fixturesByDate
+            ForEach(fixtures, id: \.self) { fixturesByDate in
+                if let date = DateFormatter.formatUTCDateToString(fixturesByDate.date) {
+                    if viewModel.shouldShowSection(fixturesByDate) {
+                        Section(header: Text(date)) {
+                            ForEach(fixturesByDate.fixtures.filter(shouldShowFixtureRow)) { fixture in
+                                FixtureRowView(
+                                    fixture: fixture,
+                                    isFavorite: viewModel.isFavorite(fixture),
+                                    toggleFavorite: { viewModel.toggleFavorite(for: fixture) }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+    
+    private func shouldShowFixtureRow(for fixture: FixtureRowViewModel) -> Bool {
+        !viewModel.showFavoritesOnly || viewModel.isFavorite(fixture)
+    }
+    
+    private var toggleFavoritesOnlyButton: some View {
+        Toggle(isOn: $viewModel.showFavoritesOnly) {
+            Text("favorites_only".localized)
+        }
+        .toggleStyle(SwitchToggleStyle(tint: .blue))
+    }
+    
+    var emptyResultView: some View {
+        NavigationView {
+            AnyView(Text("No results")
+                .font(.largeTitle)
+                .foregroundColor(.gray))
+        }
+        .navigationBarTitle("Fixture".localized)
     }
 }
 
