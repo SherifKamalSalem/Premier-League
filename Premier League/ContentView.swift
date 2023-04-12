61b41d1fdbdec49db4eaba92b0e5d0ce802c2972
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-12 20:45:08 +0200

    Add english localization

--- Premier League/ContentView.swift
+++ Premier League/ContentView.swift
@@ -6,16 +6,18 @@
 //
 
 import SwiftUI
+import Moya
 
 struct ContentView: View {
+    let provider = MoyaProvider<API>()
+    let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        FixturesListView(viewModel: FixturesListViewModel(service: FixturesListService(network: NetworkManager(provider: provider, decoder: decoder)), userDefaults: UserDefaults.standard))
     }
 }
 
