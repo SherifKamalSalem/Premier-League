f909e7b4040e21259f03426fa6255b1afd7f7c46
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-12 19:49:17 +0200

    Store the auth token in keychain and send it encrypted in the header for avoid leaking it

--- Premier League/Network/API/API.swift
+++ Premier League/Network/API/API.swift
@@ -5,14 +5,15 @@
 //  Created by Sherif Kamal on 05/04/2023.
 //
 
-import Moya
 import Foundation
+import Moya
 
 enum API {
-    case matchList(competitionId: String)
+    case featchMatchesList(competitionId: String, token: String? = SecretsUtility.token.value)
 }
 
 extension API: TargetType {
+    
     var baseURL: URL {
         guard let url = URL(string: Constants.baseURL) else { fatalError() }
         return url
@@ -20,7 +21,7 @@ extension API: TargetType {
     
     var path: String {
         switch self {
-        case let .matchList(competitionId):
+        case let .featchMatchesList(competitionId, _):
             return "competitions/\(competitionId)/matches"
         }
     }
@@ -33,14 +34,23 @@ extension API: TargetType {
         return Data()
     }
     
-    var task: Task {
+    var task: Moya.Task {
         switch self {
-        case .matchList:
+        case .featchMatchesList:
             return .requestPlain
         }
     }
     
     var headers: [String : String]? {
-        return [:]
+        var headersDict = [String: String]()
+        switch self {
+        case let .featchMatchesList(_, token):
+            if let token = token {
+                Constants.authToken = token
+                headersDict["X-Auth-Token"] = token
+            }
+            headersDict["Connection"] = "keep-alive"
+        }
+        return headersDict
     }
 }
---
+++ Premier League/Utility/SecretsUtility.swift
@@ -0,0 +1,18 @@
+//
+//  SecretsUtility.swift
+//  Premier League
+//
+//  Created by Sherif Kamal on 05/04/2023.
+//
+
+import Foundation
+
+final class SecretsUtility {
+    
+    // MARK: - Secrets
+    
+    static let token = Secret(
+        encoded: [178, 111, 157, 33, 49, 169, 157, 45, 170, 30, 60, 10, 79, 37, 15, 70, 141, 142, 137, 222, 111, 162, 182, 57, 85, 180, 176, 51, 44, 1, 227, 38],
+        cipher: [209, 90, 165, 23, 9, 203, 249, 28, 158, 120, 5, 107, 123, 16, 57, 113, 236, 235, 185, 237, 87, 144, 215, 0, 109, 134, 136, 80, 21, 101, 133, 71])
+}
+
