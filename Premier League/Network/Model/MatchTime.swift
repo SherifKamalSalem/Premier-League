f34d3ad2b10fb536e1d086a1fd5e787d08d6bb9e
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-12 20:14:39 +0200

    Create `MatchTime`, `Winner` and `Team` entities

---
+++ Premier League/Network/Model/MatchTime.swift
@@ -0,0 +1,13 @@
+//
+//  MatchTime.swift
+//  Premier League
+//
+//  Created by Sherif Kamal on 05/04/2023.
+//
+
+import Foundation
+
+// MARK: - ExtraTime
+struct MatchTime: Codable {
+    let homeTeam, awayTeam: Int?
+}
