111ee9baff40fd774a2b82436f8cd0f30028d828
Sherif Kamal <sherif.kamal@startappz.com>
2023-04-12 20:56:57 +0200

    Add `loadFixtures` function and handle storing/toggling favorites

--- Premier League/Scenes/FixturesList/ViewModel/FixturesListViewModel.swift
+++ Premier League/Scenes/FixturesList/ViewModel/FixturesListViewModel.swift
@@ -24,14 +24,65 @@ protocol FixturesListViewModelProtocol: AnyObject, ObservableObject {
 }
 
 
-class FixturesListViewModel {
+class FixturesListViewModel: FixturesListViewModelProtocol {
     private let service: Serviceable
+    private let userDefaults: UserDefaults
     private var cancellables = Set<AnyCancellable>()
+    private let favoriteFixturesKey = "favoriteFixtures"
     
     @Published var fixturesByDate = [FixturesByDate]()
+    var favoriteFixturesIds = Set<Int>()
+    var state = CurrentValueSubject<FetchState, Never>(.ideal)
+    var showError = CurrentValueSubject<Bool, Never>(false)
+    @Published var showFavoritesOnly = false
+    var errorMessage = CurrentValueSubject<String, Never>("")
     
-    init(service: Serviceable) {
+    init(service: Serviceable, userDefaults: UserDefaults) {
         self.service = service
+        self.userDefaults = userDefaults
+        loadFixtures()
+        loadFavoriteFixtures()
+    }
+    
+    // I hardcoded the `competitionId` here just for simplicity but it has to be entered by the user
+    func loadFixtures() {
+        self.state.value = .loading
+        service.fetchFixturesList(competitionId: "2021")
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                switch completion {
+                case .failure(let error):
+                    self.updateFixtures(fixturesByDate: [], state: .showError(error: error), errorMessage: error.localizedDescription)
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] response in
+                guard let self = self else { return }
+                let fixtures = response.matches
+                let fixturesByDate = sortedFixturesByDate(fixtures.map(FixtureRowViewModel.init))
+                self.updateFixtures(fixturesByDate: fixturesByDate, state: fixturesByDate.isEmpty ? .empty : .finished, errorMessage: "")
+            })
+            .store(in: &cancellables)
+    }
+    
+    func toggleFavorite(for fixture: FixtureRowViewModel) {
+        if favoriteFixturesIds.contains(fixture.id) {
+            favoriteFixturesIds.remove(fixture.id)
+        } else {
+            favoriteFixturesIds.insert(fixture.id)
+        }
+        userDefaults.set(Array(favoriteFixturesIds), forKey: favoriteFixturesKey)
+    }
+    
+    func isFavorite(_ fixture: FixtureRowViewModel) -> Bool {
+        favoriteFixturesIds.contains(fixture.id)
+    }
+    
+    func loadFavoriteFixtures() {
+        if let favoriteFixtureIds = userDefaults.array(forKey: favoriteFixturesKey) as? [Int] {
+            favoriteFixturesIds = Set(favoriteFixtureIds)
+        }
     }
     
     private func handleStoringFutureFixtures(_ sortedFutureFixtures: [FixtureRowViewModel], _ fixturesByDate: inout [FixturesByDate]) {
@@ -76,5 +127,22 @@ class FixturesListViewModel {
         
         handleStoringPastFixtures(sortedPastFixtures, &fixturesByDate)
         return fixturesByDate
+    }
+    
+    private func updateFixtures(
+        fixturesByDate: [FixturesByDate],
+        state: FetchState,
+        errorMessage: String) {
+        self.fixturesByDate = fixturesByDate
+        self.state.value = state
+        self.errorMessage.value = errorMessage
+    }
+    
+    func shouldShowSection(_ fixturesByDate: FixturesByDate) -> Bool {
+        if showFavoritesOnly {
+             return fixturesByDate.fixtures.contains(where: { isFavorite($0) })
+        } else {
+            return !fixturesByDate.fixtures.isEmpty
+        }
     }
 }
